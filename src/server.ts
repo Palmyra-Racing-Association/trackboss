67484d69c4d4f48862d667203e70b16c2b66136c
cjs8487 <cjschaefer1999@gmail.com>
2021-09-27 22:00:05 -0400

    script updates

---
+++ src/server.ts
@@ -0,0 +1,3 @@
+// this makes the compiler not complain if the file is empty
+// the tsconfig specifies a safety flag that requires all files to be modules, and empty file is not a module
+export {}
