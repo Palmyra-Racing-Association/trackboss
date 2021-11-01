54786639976462278b3163636ec0c35d111b6cca
cjs8487 <cjschaefer1999@gmail.com>
2021-11-01 01:59:12 -0400

    member endpoints

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import member from './Member';
 import membership from './Membership';
 
@@ -6,5 +6,13 @@ const router = express.Router();
 
 router.use('/member', member);
 router.use('/membership', membership);
+
+router.get('/members', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+router.post('/newMember', (req: Request, res: Response) => {
+    res.status(501).send();
+});
 
 export default router;
--- src/api/Member.ts
+++ src/api/Member.ts
@@ -1,5 +1,25 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 
 const member = express.Router();
+
+member.get('/:memberID', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+member.patch('/:memberID', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+member.delete('/:memberID', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+member.post('/:memberID/resetPassword', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+member.post('/:memberID/changePassword', (req: Request, res: Response) => {
+    res.status(501).send();
+});
 
 export default member;
