3e78c4d087d88593e9955e9532d61cb259440419
Elijah Parrish <elijah.parrish321@gmail.com>
2021-11-14 11:47:04 -0500

    Missed a couple

--- src/api/bike.ts
+++ src/api/bike.ts
@@ -10,6 +10,14 @@ bike.get('/:bikeID', (req: Request, res: Response) => {
     res.status(501).send();
 });
 
+bike.patch('/:bikeID', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
+bike.delete('/:bikeID', (req: Request, res: Response) => {
+    res.status(501).send();
+});
+
 bike.get('/list', (req: Request, res: Response) => {
     res.status(501).send();
 });
