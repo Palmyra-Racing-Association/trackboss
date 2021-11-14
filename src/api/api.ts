f5059be399e2de71458295b72feec6b4d3b6a9fd
cjs8487 <cjschaefer1999@gmail.com>
2021-11-14 18:09:02 -0500

    work points stubs

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,12 +4,14 @@ import eventJob from './eventJob';
 import job from './job';
 import member from './member';
 import membership from './membership';
+import workPoints from './workPoints';
 
 const api = Router();
 
 api.use('/member', member);
 api.use('/membership', membership);
 api.use('/job', job);
+api.use('/workPoints', workPoints);
 api.use('/eventJob', eventJob);
 api.use('/billing', billing);
 
