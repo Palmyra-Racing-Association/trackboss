8ecdb77c82d224cf4337bde80d19632a899ec5fb
Elijah Parrish <elijah.parrish321@gmail.com>
2022-01-15 13:10:34 -0500

    Unit test working with request body content

--- frontend/src/__tests__/workPoints.test.ts
+++ frontend/src/__tests__/workPoints.test.ts
@@ -3,6 +3,8 @@ import { getByMembership } from '../controller/workPoints';
 test('handler returns total', async () => {
     // This is calling our controller, but is intercepted by the MSW handler
     const res = await getByMembership(1);
-    console.log(res); // For some reason, i cant get the body from the test
+    const body = await res.json();
+
     expect(res.status).toBe(200);
+    expect(body).toEqual({ total: 3 });
 });
--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -4,7 +4,6 @@ const handlers = [
     // Handles a GET request
     rest.get('http://localhost:8080/api/workPoints/byMembership/1', (req, res, ctx) => {
         const response = res(ctx.status(200), ctx.json({ total: 3 }));
-        // The response looks good here, but not in the test
         return response;
     }),
 ];
