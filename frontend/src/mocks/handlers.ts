1689b69937858d611fc15ee7395830cd572a9f51
elijah-parrish <elijah.parrish321@gmail.com>
2022-01-20 09:19:59 -0500

    Fixing build errors

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,19 +1,19 @@
 import { rest } from 'msw';
 
 const handlers = [
-    rest.get('http://localhost:8080/api/workPoints/byMembership/1', (req, res, ctx) => {
+    rest.get(`${process.env.REACT_APP_API_URL}/api/workPoints/byMembership/1`, (req, res, ctx) => {
         const response = res(ctx.status(200), ctx.json({ total: 3 }));
         return response;
     }),
-    rest.get('http://localhost:8080/api/workPoints/byMembership/-1', (req, res, ctx) => {
+    rest.get(`${process.env.REACT_APP_API_URL}/api/workPoints/byMembership/-1`, (req, res, ctx) => {
         const response = res(ctx.status(400));
         return response;
     }),
-    rest.get('http://localhost:8080/api/workPoints/byMember/1', (req, res, ctx) => {
+    rest.get(`${process.env.REACT_APP_API_URL}/api/workPoints/byMember/1`, (req, res, ctx) => {
         const response = res(ctx.status(200), ctx.json({ total: 5 }));
         return response;
     }),
-    rest.get('http://localhost:8080/api/workPoints/byMember/-1', (req, res, ctx) => {
+    rest.get(`${process.env.REACT_APP_API_URL}/api/workPoints/byMember/-1`, (req, res, ctx) => {
         const response = res(ctx.status(400));
         return response;
     }),
