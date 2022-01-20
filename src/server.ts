6483d2f13e03304e766b2b173db8d4a1e3468796
matedro <matedro1@gmail.com>
2022-01-19 20:04:19 -0500

    Added Winston to backend

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import express from 'express';
+import { createLogger, format, transports } from 'winston';
 import api from './api/api';
 
 const app = express();
 
 const port = 8080;
 
+const { combine, timestamp: timestampFn, label: labelFn, printf } = format;
+const myFormat =
+    printf(({ level, message, label, timestamp }) => `${timestamp} [${label}] ${level}: ${message}`);
+
+const logger = createLogger({
+    format: combine(
+        labelFn({ label: 'Club Manager' }),
+        timestampFn({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        myFormat,
+    ),
+    transports: [new transports.Console()],
+});
+
 app.use('/api', api);
 app.listen(port, () => {
-    // TODO #36: replace this console.log with a logger, remove the ESLint suppressor
-    // eslint-disable-next-line no-console
-    console.log(`PRA Club Manager API listenting on port ${port}`);
+    logger.info(`PRA Club Manager API listening on port ${port}`);
 });
 
 export {};
