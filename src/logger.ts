5a026f3f0a014829dcc1b5d65ad79e32fbb4253a
matedro <matedro1@gmail.com>
2022-01-19 23:27:01 -0500

    Documentation and var rename

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,19 @@
+// Object to handle all logging in the backend
+//
+// Example log:
+// 2022-01-19 23:23:41 [Club Manager] info: PRA Club Manager API listening on port 8080
+
 import { createLogger, format, transports } from 'winston';
 
 const { combine, timestamp: timestampFn, label: labelFn, printf } = format;
-const myFormat =
+const logFormat =
     printf(({ level, message, label, timestamp }) => `${timestamp} [${label}] ${level}: ${message}`);
 
 const logger = createLogger({
     format: combine(
         labelFn({ label: 'Club Manager' }),
         timestampFn({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        myFormat,
+        logFormat,
     ),
     transports: [new transports.Console()],
 });
