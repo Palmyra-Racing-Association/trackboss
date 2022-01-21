d3d73016c83fd67d3e9dc9234c80e39db92a6cac
elijah-parrish <elijah.parrish321@gmail.com>
2022-01-20 19:36:36 -0500

    Added error handling utility function

--- frontend/src/__tests__/workPoints.test.ts
+++ frontend/src/__tests__/workPoints.test.ts
@@ -1,21 +1,25 @@
 import { getByMembership, getByMember } from '../controller/workPoints';
 
 test('getByMembership returns total with valid id', async () => {
-    const res = await getByMembership(1);
+    const token = 'TestingToken';
+    const res = await getByMembership(token, 1);
     expect(res.total).toEqual(3);
 });
 
 test('getByMembership returns error with invalid id', async () => {
-    const res = await getByMembership(-1);
-    expect(res).toEqual('error: endpoint not found');
+    const token = 'TestingToken';
+    const res = await getByMembership(token, -1);
+    expect(res).toEqual('Bad request');
 });
 
 test('getByMember returns total', async () => {
-    const res = await getByMember(1);
+    const token = 'TestingToken';
+    const res = await getByMember(token, 1);
     expect(res.total).toEqual(5);
 });
 
 test('getByMembership returns error with invalid id', async () => {
-    const res = await getByMember(-1);
-    expect(res).toEqual('error: endpoint not found');
+    const token = 'TestingToken';
+    const res = await getByMember(token, -1);
+    expect(res).toEqual('Bad request');
 });
--- frontend/src/controller/workPoints.ts
+++ frontend/src/controller/workPoints.ts
@@ -1,21 +1,31 @@
-export async function getByMember(memberId: Number) {
+import errorHandler from './utils';
+
+export async function getByMember(token: String, memberId: Number) {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workPoints/byMember/${memberId}`, {
         method: 'GET',
         mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
     });
     if (response.status === 200) {
         return response.json();
     }
-    return 'error: endpoint not found';
+    return errorHandler(response);
 }
 
-export async function getByMembership(membershipId: Number) {
+export async function getByMembership(token: String, membershipId: Number) {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workPoints/byMembership/${membershipId}`, {
         method: 'GET',
         mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
     });
     if (response.status === 200) {
         return response.json();
     }
-    return 'error: endpoint not found';
+    return errorHandler(response);
 }
