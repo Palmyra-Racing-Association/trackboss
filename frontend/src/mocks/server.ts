f4586697de2ba612f6e015a59e06675802a388b5
Elijah Parrish <elijah.parrish321@gmail.com>
2022-01-27 02:22:25 -0500

    Didnt miss a typo...

--- frontend/src/mocks/server.ts
+++ frontend/src/mocks/server.ts
@@ -4,6 +4,6 @@ import workPointsHandlers from './workPointsHandlers';
 import membershipHandlers from './membershipHandlers';
 
 // This configures a request mocking server with the given request handlers.
-const server = setupServer(...workPointsHandlers, ...membershipHandlers);
+const server = setupServer(...membershipHandlers, ...workPointsHandlers);
 
 export { server, rest };
