24d9be87c5a6b855b8f4528b63d8bd73d945f5af
Brianna <bnf8555@rit.edu>
2022-01-27 13:31:13 -0500

    es lint

--- frontend/src/__tests__/snapshots/HamburgerMenu.test.tsx
+++ frontend/src/__tests__/snapshots/HamburgerMenu.test.tsx
@@ -3,12 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import HamburgerMenu from '../../components/HamburgerMenu';
 
-
 it('renders correctly with no page selected', () => {
     const menu = renderer.create(
         <BrowserRouter>
-            <HamburgerMenu activeButtonId={0}/>
-        </BrowserRouter>
+            <HamburgerMenu activeButtonId={0} />
+        </BrowserRouter>,
     ).toJSON();
     expect(menu).toMatchSnapshot();
 });
@@ -16,8 +15,8 @@ it('renders correctly with no page selected', () => {
 it('renders correctly with Dashboard selected', () => {
     const menu = renderer.create(
         <BrowserRouter>
-            <HamburgerMenu activeButtonId={1}/>
-        </BrowserRouter>
+            <HamburgerMenu activeButtonId={1} />
+        </BrowserRouter>,
     ).toJSON();
     expect(menu).toMatchSnapshot();
 });
@@ -25,8 +24,8 @@ it('renders correctly with Dashboard selected', () => {
 it('renders correctly with Calendar selected', () => {
     const menu = renderer.create(
         <BrowserRouter>
-            <HamburgerMenu activeButtonId={2}/>
-        </BrowserRouter>
+            <HamburgerMenu activeButtonId={2} />
+        </BrowserRouter>,
     ).toJSON();
     expect(menu).toMatchSnapshot();
 });
@@ -34,8 +33,8 @@ it('renders correctly with Calendar selected', () => {
 it('renders correctly with Members selected', () => {
     const menu = renderer.create(
         <BrowserRouter>
-            <HamburgerMenu activeButtonId={3}/>
-        </BrowserRouter>
+            <HamburgerMenu activeButtonId={3} />
+        </BrowserRouter>,
     ).toJSON();
     expect(menu).toMatchSnapshot();
 });
@@ -43,8 +42,8 @@ it('renders correctly with Members selected', () => {
 it('renders correctly with My Account selected', () => {
     const menu = renderer.create(
         <BrowserRouter>
-            <HamburgerMenu activeButtonId={4}/>
-        </BrowserRouter>
+            <HamburgerMenu activeButtonId={4} />
+        </BrowserRouter>,
     ).toJSON();
     expect(menu).toMatchSnapshot();
 });
--- frontend/src/__tests__/snapshots/Header.test.tsx
+++ frontend/src/__tests__/snapshots/Header.test.tsx
@@ -3,42 +3,38 @@ import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Header from '../../components/Header';
 
-it("renders correctly for Dashboard", () => {
+it('renders correctly for Dashboard', () => {
     const header = renderer.create(
         <BrowserRouter>
-            <Header title='Dashboard' activeButtonId={1}/>
-        </BrowserRouter>
+            <Header title="Dashboard" activeButtonId={1} />
+        </BrowserRouter>,
     ).toJSON();
-    
     expect(header).toMatchSnapshot();
 });
 
-it("renders correctly for Calendar", () => {
+it('renders correctly for Calendar', () => {
     const header = renderer.create(
         <BrowserRouter>
-            <Header title='Calendar' activeButtonId={2}/>
-        </BrowserRouter>
+            <Header title="Calendar" activeButtonId={2} />
+        </BrowserRouter>,
     ).toJSON();
-    
     expect(header).toMatchSnapshot();
 });
 
-it("renders correctly for Member List", () => {
+it('renders correctly for Member List', () => {
     const header = renderer.create(
         <BrowserRouter>
-            <Header title='Members' activeButtonId={3}/>
-        </BrowserRouter>
+            <Header title="Members" activeButtonId={3} />
+        </BrowserRouter>,
     ).toJSON();
-    
     expect(header).toMatchSnapshot();
 });
 
-it("renders correctly for Settings", () => {
+it('renders correctly for Settings', () => {
     const header = renderer.create(
         <BrowserRouter>
-            <Header title='General Info' activeButtonId={4}/>
-        </BrowserRouter>
+            <Header title="General Info" activeButtonId={4} />
+        </BrowserRouter>,
     ).toJSON();
-    
     expect(header).toMatchSnapshot();
 });
