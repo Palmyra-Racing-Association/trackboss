fad242a372b53e91265acb5c14fcfdb66253c088
Brianna <bnf8555@rit.edu>
2022-01-27 15:16:33 -0500

    endpoints hit and tests created

--- frontend/src/controller/utils.ts
+++ frontend/src/controller/utils.ts
@@ -1,6 +1,14 @@
-export default function generateHeaders(token: string) {
+export default function generateHeaders(token: string, range?: string) {
+    if(typeof range !== 'undefined') {
+        return ({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        });
+    }
     return ({
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
+        Range: `${range}`,
     });
 }
+``
