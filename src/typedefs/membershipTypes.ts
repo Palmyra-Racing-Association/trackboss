85c7e717e5e9b4268e6624f8700e484795cf0d8d
cjs8487 <cjschaefer1999@gmail.com>
2022-01-26 23:11:04 -0500

    membership types

---
+++ src/typedefs/membershipTypes.ts
@@ -0,0 +1,73 @@
+import { ErrorResponse } from './errorResponse';
+
+export type Membership = {
+    membershipId: number,
+    membershipAdmin: string,
+    active: boolean,
+    curYearRenewed: boolean,
+    renewalSent: boolean,
+    yearJoined: number,
+    address: string,
+    city: string
+    state: string,
+    zip: string,
+    lastModifiedDate: Date,
+    lastModifiedBy: string
+}
+
+export type PostNewMembershipRequest = {
+    yearJoined: number,
+    address: string,
+    city: string,
+    state: string,
+    zip: string,
+    modifiedBy: number
+}
+
+export type PostNewMembershipResposne = Membership | ErrorResponse
+
+export type GetMembershipRequest = {}
+
+export type GetMembershipResponse = Membership | ErrorResponse
+
+export type PatchMembershipRequest = {
+    address: string,
+    city: string,
+    state: string,
+    zi: number,
+    modifiedBy: number
+}
+
+export type PatchMemberResponse = Membership | ErrorResponse
+
+export type GetMembershipListRequest = {}
+
+export type GetMembershipListResponse = Membership[]
+
+export type PostRegisterNewMembershipRequest = {
+    memberTypeId: number,
+    firstName: string,
+    lastName: string,
+    phoneNumber: string,
+    occupation: string,
+    email: string,
+    birthdate: string,
+    address: string,
+    city: string,
+    state: string,
+    zip: string
+}
+
+export type PostRegisterMembershipResponse = {
+    memberType: string,
+    firstName: string,
+    lastName: string,
+    phoneNumber: string,
+    occupation: string,
+    email: string,
+    birthdate: string,
+    address: string,
+    city: string,
+    state: string,
+    zip: string
+}
