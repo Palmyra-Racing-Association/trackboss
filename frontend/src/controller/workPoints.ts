97a1bc76a6e830fe795fcef1f12f20b11594e535
matedro <matedro1@gmail.com>
2022-01-27 04:04:48 -0500

    Is this what was intended for the typedefs in the controller?

--- frontend/src/controller/workPoints.ts
+++ frontend/src/controller/workPoints.ts
@@ -1,6 +1,7 @@
 import generateHeaders from './utils';
+import { GetMembershipWorkPointsResponse, GetMemberWorkPointsResponse } from '../../../src/typedefs/workPoints';
 
-export async function getByMember(token: string, memberId: number) {
+export async function getByMember(token: string, memberId: number): Promise<GetMemberWorkPointsResponse> {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workPoints/byMember/${memberId}`, {
         method: 'GET',
         mode: 'no-cors',
@@ -9,7 +10,7 @@ export async function getByMember(token: string, memberId: number) {
     return response.json();
 }
 
-export async function getByMembership(token: string, membershipId: number) {
+export async function getByMembership(token: string, membershipId: number): Promise<GetMembershipWorkPointsResponse> {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workPoints/byMembership/${membershipId}`, {
         method: 'GET',
         mode: 'no-cors',
