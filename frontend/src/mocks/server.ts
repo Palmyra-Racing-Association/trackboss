901bee5249e0fbf2f1080595db5b157706d015c7
cjs8487 <cjschaefer1999@gmail.com>
2022-01-27 15:08:21 -0500

    eventtype controller

--- frontend/src/mocks/server.ts
+++ frontend/src/mocks/server.ts
@@ -3,8 +3,10 @@ import { setupServer } from 'msw/node';
 import workPointsHandlers from './workPointsHandlers';
 import membershipHandlers from './membershipHandlers';
 import memberHandlers from './memberHandlers';
+import eventTypeHandlers from './eventTypeHandlers';
 
 // This configures a request mocking server with the given request handlers.
-const server = setupServer(...membershipHandlers, ...workPointsHandlers, ...memberHandlers);
+const server = setupServer(...membershipHandlers, ...workPointsHandlers,
+    ...memberHandlers, ...eventTypeHandlers);
 
 export { server, rest };
