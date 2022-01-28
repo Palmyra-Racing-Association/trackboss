0811dfe49f0059fab74a977b2278bffad1af505e
Elijah Parrish <elijah.parrish321@gmail.com>
2022-01-27 21:28:22 -0500

    fixing merge conflicts

--- frontend/src/mocks/server.ts
+++ frontend/src/mocks/server.ts
@@ -1,20 +1,17 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import bikeHandlers from './bikeHandlers';
 import eventHandlers from './eventHandlers';
 import eventJobHandlers from './eventJobHandlers';
 import eventTypeHandlers from './eventTypeHandlers';
-import memberHandlers from './memberHandlers';
 import membershipHandlers from './membershipHandlers';
 import jobHandlers from './jobHandlers';
 import jobTypeHandlers from './jobTypeHandlers';
 import workPointsHandlers from './workPointsHandlers';
 import bikeHandlers from './bikeHandlers';
-import memberHandlers from './memberHandlers;
+import memberHandlers from './memberHandlers';
 
 // This configures a request mocking server with the given request handlers.
 const server = setupServer(
-    ...bikeHandlers,
     ...eventTypeHandlers,
     ...eventJobHandlers,
     ...eventHandlers,
@@ -24,7 +21,6 @@ const server = setupServer(
     ...jobTypeHandlers,
     ...workPointsHandlers,
     ...bikeHandlers,
-    ...memberHandlers,
 );
 
 export { server, rest };
