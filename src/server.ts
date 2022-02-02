5b2ece82258df605f7907080c339b00bd5ad6406
jjirinec <jirinecj@gmail.com>
2022-02-02 17:17:20 -0500

    Remove test function from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import api from './api/api';
-import { patchBike } from './database/bike';
 import logger from './logger';
 
 const app = express();
@@ -11,13 +10,7 @@ app.use('/api', api);
 const server = app.listen(port, () => {
     logger.info(`PRA Club Manager API listening on port ${port}`);
 });
-const fn = async () => {
-    const id = 104;
-    const req = {};
-    //logger.info(`${JSON.stringify(await patchBike(id, req))}`);
-};
 
-fn();
 
 // export the HTTP server so that it can be closed if necessary (mostly for testing)
 export default server;
