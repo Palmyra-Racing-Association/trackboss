a417599226eb363c380fe250da24e911112b2fe4
Elijah Parrish <elijah.parrish321@gmail.com>
2022-02-02 19:14:43 -0500

    testing actually done

--- frontend/src/__tests__/snapshots/ImportantLinksCard.test.tsx
+++ frontend/src/__tests__/snapshots/ImportantLinksCard.test.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import ImportantLinksCard from '../../components/ImportantLinksCard';
 
-it('renders correctly for Dashboard', () => {
-    const importantLinksCard = renderer.create(
+it('header renders correctly', () => {
+    render(
         <BrowserRouter>
             <ImportantLinksCard />
         </BrowserRouter>,
     );
-    expect(importantLinksCard).toMatchSnapshot();
+    expect(screen.getByText('Important Links')).toBeInTheDocument();
+});
+
+it('member rules link renders correctly', () => {
+    render(
+        <BrowserRouter>
+            <ImportantLinksCard />
+        </BrowserRouter>,
+    );
+    expect(screen.getByText('Member Rules').closest('a'))
+        .toHaveAttribute('href', 'https://palmyramx.com/pages/track-rules');
+});
+
+it('palmyra racing twitter link renders correctly', () => {
+    render(
+        <BrowserRouter>
+            <ImportantLinksCard />
+        </BrowserRouter>,
+    );
+    expect(screen.getByText('Twitter').closest('a'))
+        .toHaveAttribute('href', 'https://twitter.com/palmyramx');
+});
+
+it('palmyra racing facebook link renders correctly', () => {
+    render(
+        <BrowserRouter>
+            <ImportantLinksCard />
+        </BrowserRouter>,
+    );
+    expect(screen.getByText('Facebook').closest('a'))
+        .toHaveAttribute('href', 'https://www.facebook.com/palmyramx/');
+});
+
+it('palmyra racing instagram link renders correctly', () => {
+    render(
+        <BrowserRouter>
+            <ImportantLinksCard />
+        </BrowserRouter>,
+    );
+    expect(screen.getByText('Instagram').closest('a'))
+        .toHaveAttribute('href', 'https://www.instagram.com/palmyramx_hogback_hill/?hl=en');
 });
