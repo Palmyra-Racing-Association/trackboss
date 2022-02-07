674842c7c9d37b9cdbba32487638f221a7090cdc
cjs8487 <cjschaefer1999@gmail.com>
2022-02-07 14:51:29 -0500

    script updates

--- database/mock_data/reset_db.sh
+++ database/mock_data/reset_db.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 mysql --user=dev --password=devpass -e "source DropDB.sql"
 mysql --user=dev --password=devpass -e "source InitSchema.sql"
-for filename in procedures/*.sql
-do
-    mysql --user=dev --password=devpass pradb -e "source $filename"
-done
+./load_procedures.sh
 mysql --user=dev --password=devpass pradb -e "source AutoJobGeneration.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB_Part2.sql"
