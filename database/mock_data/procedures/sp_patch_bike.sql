132d7cdf791ad720a4ae7d71f03d7fce36adf4ab
cjs8487 <cjschaefer1999@gmail.com>
2022-02-06 19:29:44 -0500

    script updates

---
+++ database/mock_data/procedures/sp_patch_bike.sql
@@ -0,0 +1,20 @@
+CREATE DEFINER=`dev`@`localhost` PROCEDURE `sp_patch_bike`(
+IN _bike_id INT,
+IN _year VARCHAR(50),
+IN _make VARCHAR(50),
+IN _model VARCHAR(50),
+IN _membership_id INT
+)
+BEGIN
+	SELECT year, make, model, membership_id
+    INTO @cur_year, @cur_make, @cur_model, @cur_membership_id
+    FROM member_bikes b
+    WHERE bike_id = _bike_id;
+    
+    UPDATE member_bikes SET
+		year = IFNULL(_year, @cur_year),
+        make = IFNULL(_make, @cur_make),
+        model = IFNULL(_model, @cur_model),
+        membership_id = IFNULL(_membership_id, @cur_membership_id)
+	WHERE bike_id = _bike_id;
+END
--- database/mock_data/reset_db.sh
+++ database/mock_data/reset_db.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mysql --user=dev --password=devpass -e "source DropDB.sql"
 mysql --user=dev --password=devpass -e "source InitSchema.sql"
+for filename in procedures/*.sql
+do
+    mysql --user=dev --password=devpass -e "source procedures/$filename"
+done
 mysql --user=dev --password=devpass pradb -e "source AutoJobGeneration.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB_Part2.sql"
