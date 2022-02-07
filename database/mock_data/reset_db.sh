bc9d895c4e7898872b32eb25082e171b6ceb4a77
cjs8487 <cjschaefer1999@gmail.com>
2022-02-07 14:52:15 -0500

    moved the job generation script into the procedures folder

--- database/mock_data/reset_db.sh
+++ database/mock_data/reset_db.sh
@@ -2,6 +2,5 @@
 mysql --user=dev --password=devpass -e "source DropDB.sql"
 mysql --user=dev --password=devpass -e "source InitSchema.sql"
 ./load_procedures.sh
-mysql --user=dev --password=devpass pradb -e "source AutoJobGeneration.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB.sql"
 mysql --user=dev --password=devpass pradb -e "source PopulateDB_Part2.sql"
