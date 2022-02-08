5a505cf2021b33da1322ad768d57ce6231a31132
matedro <matedro1@gmail.com>
2022-02-08 11:34:20 -0500

    Added procs for membership

---
+++ database/mock_data/procedures/v_membership.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+CREATE VIEW `v_membership` AS
+    SELECT 
+        ms.membership_id,
+        CONCAT(ma.first_name, ' ', ma.last_name) AS membership_admin,
+        ms.status,
+        ms.cur_year_renewed,
+        ms.renewal_sent,
+        ms.year_joined,
+        ms.address,
+        ms.city,
+        ms.state,
+        ms.zip,
+        DATE_FORMAT(ms.last_modified_date, '%Y-%m-%d') AS last_modified_date,
+        ms.last_modified_by
+    FROM
+        membership ms
+            LEFT JOIN
+        member ma ON ms.membership_admin_id = ma.member_id
+//
