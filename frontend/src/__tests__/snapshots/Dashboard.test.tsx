40bc064f6bcbf317beb36b1f3a59d6db95ececbf
Brianna <bnf8555@rit.edu>
2022-02-07 19:41:05 -0500

    AAAAAAAAA

--- frontend/src/__tests__/snapshots/Dashboard.test.tsx
+++ frontend/src/__tests__/snapshots/Dashboard.test.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Dashboard from '../../pages/Dashboard';
+import Dashboard  from '../../pages/Dashboard';
+import WorkPointsCard from '../../components/WorkPointsCard';
+
+jest.mock('react-gauge-chart', () => {
+    return {
+      'default': 'GaugeChart'
+    }
+  });
+
 
 it('renders correctly', () => {
     const dash = renderer.create(
--- frontend/src/__tests__/snapshots/WorkPointsCard.test.tsx
+++ frontend/src/__tests__/snapshots/WorkPointsCard.test.tsx
@@ -3,6 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import WorkPointsCard from '../../components/WorkPointsCard';
 
+jest.mock('react-gauge-chart', () => {
+    return {
+      'default': 'GaugeChart'
+    }
+  });
+
+
 it('renders correctly', () => {
     const workPointsCard = renderer.create(
             <WorkPointsCard percent={67} />
