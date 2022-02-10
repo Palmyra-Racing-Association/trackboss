4e1296b7cc992b5653bfb8c0cf6138f542096318
jjirinec <jirinecj@gmail.com>
2022-02-09 21:20:36 -0500

    Patch Procedure Done

---
+++ database/mock_data/procedures/sp_patch_event.sql
@@ -0,0 +1,27 @@
+DELIMITER //
+DROP PROCEDURE IF EXISTS `sp_patch_event`;
+CREATE PROCEDURE `sp_patch_event`(
+IN _event_id INT,
+IN _date DATE,
+IN _event_name VARCHAR(255),
+IN _event_description VARCHAR(255)
+)
+BEGIN
+	SELECT date, event_name, event_description
+    INTO @date, @event_name, @event_description
+    FROM event e
+    WHERE event_id = _event_id;
+	
+	SELECT DATEDIFF(IFNULL(_date, @date), @date) INTO @date_dif;
+    
+	UPDATE job SET
+		job_date = DATE_ADD(job_date, INTERVAL @date_dif DAY),
+        last_modified_date = CURDATE()
+	WHERE event_id = _event_id;
+    
+    UPDATE event SET
+		date = IFNULL(_date, @date),
+        event_name = IFNULL(_event_name, @event_name),
+        event_description = IFNULL(_event_description, @event_description)
+	WHERE event_id = _event_id;
+END//
