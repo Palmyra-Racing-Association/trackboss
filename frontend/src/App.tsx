b191d90300cbecffe87f71b55ea8d37887f61f5b
cjs8487 <cjschaefer1999@gmail.com>
2022-02-11 15:14:03 -0500

    full flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,41 @@
 /*eslint-disable*/
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { UserContextProvider } from './contexts/UserContext'
+import React, { useContext, useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { UserContext, UserContextProvider } from './contexts/UserContext'
 import Dashboard from './pages/Dashboard';
 import MemberListPage from './pages/MemberListPage';
 import Settings from './pages/Settings';
 import CalendarPage from './pages/CalendarPage';
 
-export default function App() {
+const App =() => {
+    const { state, update } = useContext(UserContext);
+    useEffect(() => {
+        if (!state.loggedIn) {
+            const token = location.hash.split("#id_token=")[1]
+            if (token) {
+                console.log('logging in')
+                update({ loggedIn: true, token })
+            } else {
+                window.location.href='https://palmyramx-test.auth.us-east-1.amazoncognito.com/login?client_id=3q7ile80d2qk4bgt75af9684bq&response_type=token&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000'
+            }
+        }
+    }, [state]);
+    
     return (
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="calendar" element={<CalendarPage />} />
-                <Route path="members" element={<MemberListPage />} />
-                <Route path="settings" element={<Settings />} />
-            </Routes>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="calendar" element={<CalendarPage />} />
+            <Route path="members" element={<MemberListPage />} />
+            <Route path="settings" element={<Settings />} />
+        </Routes>
     );
+}
+
+
+export default function AppWrapper() {
+    return (
+        <UserContextProvider>
+            <App />
+        </UserContextProvider>
+    )
 }
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Button, Center, ChakraProvider, HStack, VStack } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Center, ChakraProvider, HStack, VStack } from '@chakra-ui/react';
 import theme from '../theme';
 import Header from '../components/Header';
 import WorkPointsCard from '../components/WorkPointsCard';
@@ -10,7 +10,6 @@ import { Event } from '../../../src/typedefs/event';
 import { getWorkPointsPercentage } from '../controller/workPoints';
 import GreetingText from '../components/GreetingText';
 import { getName } from '../controller/member';
-import { UserContext } from '../contexts/UserContext';
 
 async function getUpcomingEventDataLocal(): Promise<any> {
     const props = await getUpcomingEventData();
@@ -31,8 +30,6 @@ function Dashboard() {
     const [nextEvent, setNextEvent] = useState<Event | null >(null);
     const [percent, setPercent] = useState(0);
     const [memberName, setMemberName] = useState('');
-    const { state, update } = useContext(UserContext);
-    console.log(state);
     useEffect(() => {
         async function getData() {
             const per = await getWorkPointsPercentageLocal();
@@ -64,9 +61,6 @@ function Dashboard() {
                             )
                         }
                         <ImportantLinksCard />
-                        <Button active onClick={() => update({ loggedIn: true, token: 'test' })}>
-                            Log In
-                        </Button>
                     </HStack>
                 </Center>
             </VStack>
