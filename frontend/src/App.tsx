5735738cd560499f63c96ddce8f51395fe4a1b2d
cjs8487 <cjschaefer1999@gmail.com>
2022-02-12 17:18:59 -0500

    upgraded url determination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,29 @@
-/*eslint-disable*/
 import React, { useContext, useEffect } from 'react';
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import { UserContext, UserContextProvider } from './contexts/UserContext'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { UserContext, UserContextProvider } from './contexts/UserContext';
 import Dashboard from './pages/Dashboard';
 import MemberListPage from './pages/MemberListPage';
 import Settings from './pages/Settings';
 import CalendarPage from './pages/CalendarPage';
 
-const App =() => {
+const App = () => {
     const { state, update } = useContext(UserContext);
+    const location = useLocation();
     useEffect(() => {
         if (!state.loggedIn) {
-            const hash = location.hash.split("#id_token=")[1]
+            const hash = location.hash.split('#id_token=')[1];
             if (hash) {
-                const token = hash.split('&')[0]
-                console.log('logging in')
-                update({ loggedIn: true, token })
+                const token = hash.split('&')[0];
+                update({ loggedIn: true, token });
             } else {
-                window.location.href='https://palmyramx-test.auth.us-east-1.amazoncognito.com/login?client_id=3q7ile80d2qk4bgt75af9684bq&response_type=token&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000'
+                // this is the only reasonable way to do this other than repeated string concatenation
+                // eslint-disable-next-line max-len
+                const authTarget = `${process.env.REACT_APP_AUTH_URL}/login?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&scope=email+openid+phone&redirect_uri=${window.location.origin}`;
+                window.location.href = authTarget;
             }
         }
     }, [state]);
-    
+
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
@@ -30,13 +32,12 @@ const App =() => {
             <Route path="settings" element={<Settings />} />
         </Routes>
     );
-}
-
+};
 
 export default function AppWrapper() {
     return (
         <UserContextProvider>
             <App />
         </UserContextProvider>
-    )
+    );
 }
