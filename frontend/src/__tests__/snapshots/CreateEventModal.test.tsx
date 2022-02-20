86df48697a7a41bdd365cd4f0c7c407d656e99cd
Brianna <bnf8555@rit.edu>
2022-02-19 20:18:12 -0500

    mock date.now

--- frontend/src/__tests__/snapshots/CreateEventModal.test.tsx
+++ frontend/src/__tests__/snapshots/CreateEventModal.test.tsx
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 import CreateEventModal from '../../components/CreateEventModal';
 import '@testing-library/jest-dom/extend-expect';
 
+Date.now = jest.fn(() => 1487076708000)
+
 it('renders all props correctly', () => {
     const modal = renderer.create(
         <CreateEventModal />,
