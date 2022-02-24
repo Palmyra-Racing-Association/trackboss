f8779851b862d20d13d3a829bfc770ace2e8273c
cjs8487 <cjschaefer1999@gmail.com>
2022-02-23 23:11:54 -0500

    updated tests

--- frontend/src/__tests__/snapshots/App.test.tsx
+++ frontend/src/__tests__/snapshots/App.test.tsx
@@ -1,55 +1,51 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { act, create } from 'react-test-renderer';
+import { act, create, ReactTestRenderer } from 'react-test-renderer';
 import 'jest-location-mock';
-import ReactDOM, { render } from 'react-dom';
 import { App, AppWrapper } from '../../App';
 
 jest.mock('react-gauge-chart', () => 'GaugeChart');
 
-let container: Node | null;
-
-beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-});
-
-afterEach(() => {
-    if (container !== null) {
-        document.body.removeChild(container);
-    }
-    container = null;
-});
-
 describe('AppWrapper', () => {
     it('renders correctly', () => {
-        const appWrapper = create(
-            <BrowserRouter>
-                <AppWrapper />
-            </BrowserRouter>,
-        ).toJSON();
-        expect(appWrapper).toMatchSnapshot();
+        let appWrapper: ReactTestRenderer;
+        act(() => {
+            appWrapper = create(
+                <BrowserRouter>
+                    <AppWrapper />
+                </BrowserRouter>,
+            );
+        });
+        expect(appWrapper!.toJSON()).toMatchSnapshot();
     });
 });
 
 describe('App', () => {
     it('redirects correctly', () => {
         expect(window.location.href).toBe('http://localhost/');
-        create(
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>,
-        );
+        act(() => {
+            create(
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>,
+            );
+        });
         expect(window.location.href).toBe(
             // eslint-disable-next-line max-len
-            `${process.env.REACT_APP_AUTH_URL}/login?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&scope=email+openid+phone&redirect_uri=${window.location.origin}`
+            `${process.env.REACT_APP_AUTH_URL}/login?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&scope=email+openid+phone&redirect_uri=http://localhost`,
         );
     });
 
     it('correctly parses a key after a redirect', () => {
         window.location.hash = 'id_token=testing.tok.en&auth_token=iamanauthtoken&expires_in=3600&token_type=Bearer';
+        let app: ReactTestRenderer;
         act(() => {
-            render(<App />, container);
+            app = create(
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>,
+            );
         });
+        expect(app!.toJSON).toMatchSnapshot();
     });
 });
