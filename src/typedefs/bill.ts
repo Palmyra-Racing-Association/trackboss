a52c233b44c4b45287d66f01e58ba1069c638db6
matedro <matedro1@gmail.com>
2022-02-24 02:16:02 -0500

    Billing database functions

--- src/typedefs/bill.ts
+++ src/typedefs/bill.ts
@@ -23,11 +23,25 @@ export type GetWorkPointThresholdResponse = WorkPointThreshold | ErrorResponse
 
 export type GetBillListRequest = {}
 
+// Used only for the backend: API layer -> DB layer communication
+export type GetBillListRequestFilters = {
+    membershipId?: number,
+    year?: number,
+    paymentStatus?: string
+}
+
 export type GetBillListResponse = Bill[] | ErrorResponse
 
 export type PostCalculateBillsRequest = {}
 
 export type PostCalculateBillsResponse = Bill[] | ErrorResponse
+
+// Used only for the backend: API layer -> DB layer communication
+export type GenerateSingleBillRequest = {
+    amount: number,
+    amountWithFee: number,
+    membershipId: number
+}
 
 export type GetMembershipBillListRequest = {}
 
