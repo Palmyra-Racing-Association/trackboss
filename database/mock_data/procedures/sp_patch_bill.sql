e1f53d2a38c1effa4c7215a316f251b18ab3acf1
matedro <matedro1@gmail.com>
2022-02-24 01:55:13 -0500

    Actually, tweaked bill proc and tests

--- database/mock_data/procedures/sp_patch_bill.sql
+++ database/mock_data/procedures/sp_patch_bill.sql
@@ -1,21 +1,17 @@
 DELIMITER //
 CREATE PROCEDURE sp_patch_bill(
     IN _bill_id INT,
-    IN _amount DOUBLE,
-    IN _amount_with_fee DOUBLE,
     IN _emailed_bill DATE,
     IN _cur_year_paid BIT
 )
 BEGIN
-    SELECT amount, amount_with_fee, emailed_bill, cur_year_paid
-    INTO @cur_amount, @cur_amount_with_fee, @cur_emailed_bill, @cur_cur_year_paid
+    SELECT emailed_bill, cur_year_paid
+    INTO @cur_emailed_bill, @cur_cur_year_paid
     FROM member_bill
     WHERE bill_id = _bill_id;
 
 UPDATE member_bill 
 SET 
-    amount = IFNULL(_amount, @cur_amount),
-    amount_with_fee = IFNULL(_amount_with_fee, @cur_amount_with_fee),
     emailed_bill = IFNULL(_emailed_bill, @cur_emailed_bill),
     cur_year_paid = IFNULL(_cur_year_paid, @cur_cur_year_paid)
 WHERE
--- src/__tests__/database/procedures/billing.test.ts
+++ src/__tests__/database/procedures/billing.test.ts
@@ -5,7 +5,7 @@ import config from './config';
 
 const pool = mysql.createPool(config);
 
-const PROC_SQL = 'CALL sp_patch_bill(?, ?, ?, ?, ?)';
+const PROC_SQL = 'CALL sp_patch_bill(?, ?, ?)';
 const CHECK_SQL = 'SELECT bill_id, DATE_FORMAT(mb.generated_date, "%Y-%m-%d") AS generated_date, year, amount, ' +
     'amount_with_fee, membership_id, emailed_bill, cur_year_paid FROM member_bill mb WHERE bill_id = ?';
 
@@ -16,8 +16,6 @@ afterAll(async () => {
 /*
 For reference, the procedure's fields are in this order:
 - bill_id,
-- amount,
-- amount_with_fee,
 - emailed_bill,
 - cur_year_paid,
 */
@@ -27,16 +25,8 @@ describe('sp_patch_bill()', () => {
         const billId = 1;
         const [expResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
 
-        const values = [
-            billId,
-            199.99,
-            209.99,
-            '2022-02-24',
-            1,
-        ];
+        const values = [billId, '2022-02-24', 1];
 
-        expResults[0].amount = 199.99;
-        expResults[0].amount_with_fee = 209.99;
         expResults[0].emailed_bill = '2022-02-24';
         expResults[0].cur_year_paid = [1];
 
@@ -49,49 +39,11 @@ describe('sp_patch_bill()', () => {
         expect(_.isEqual(checkResults[0], expResults));
     });
 
-    it('Patches amount field', async () => {
+    it('Patches emailed_bill field', async () => {
         const billId = 2;
         const [expResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
 
-        const values = Array(5).fill(null);
-        values[0] = billId;
-        values[1] = 9999.99;
-
-        expResults[0].amount = 9999.99;
-
-        const [result] = await pool.query<OkPacket>(PROC_SQL, values);
-        expect(result.affectedRows).toBe(1);
-
-        const [checkResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
-        expect(!_.isEmpty(checkResults));
-        expect(_.isEqual(checkResults[0], expResults[0]));
-    });
-
-    it('Patches amount_with_fee field', async () => {
-        const billId = 3;
-        const [expResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
-
-        const values = Array(5).fill(null);
-        values[0] = billId;
-        values[2] = 214.1;
-
-        expResults[0].amount_with_fee = 214.1;
-
-        const [result] = await pool.query<OkPacket>(PROC_SQL, values);
-        expect(result.affectedRows).toBe(1);
-
-        const [checkResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
-        expect(!_.isEmpty(checkResults));
-        expect(_.isEqual(checkResults[0], expResults[0]));
-    });
-
-    it('Patches emailed_bill field', async () => {
-        const billId = 4;
-        const [expResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
-
-        const values = Array(5).fill(null);
-        values[0] = billId;
-        values[3] = '1900-01-02';
+        const values = [billId, '1900-01-02', null];
 
         expResults[0].emailed_bill = '1900-01-02';
 
@@ -104,12 +56,10 @@ describe('sp_patch_bill()', () => {
     });
 
     it('Patches cur_year_paid field', async () => {
-        const billId = 5;
+        const billId = 3;
         const [expResults] = await pool.query<RowDataPacket[]>(CHECK_SQL, [billId]);
 
-        const values = Array(5).fill(null);
-        values[0] = billId;
-        values[4] = 0;
+        const values = [billId, null, 0];
 
         expResults[0].cur_year_paid = 0;
 
@@ -122,7 +72,7 @@ describe('sp_patch_bill()', () => {
     });
 
     it('Throws on improper user input', async () => {
-        const billId = 6;
+        const billId = 4;
         const values = Array(5).fill(null);
         values[0] = billId;
         values[1] = 'notADoubleOrEvenANumberAtAll';
@@ -131,14 +81,14 @@ describe('sp_patch_bill()', () => {
     });
 
     it('Patches nothing without billId', async () => {
-        const values = Array(5).fill(null);
+        const values = Array(3).fill(null);
 
         const [result] = await pool.query<OkPacket>(PROC_SQL, values);
         expect(result.affectedRows).toBe(0);
     });
 
     it('Patches nothing when billId not found', async () => {
-        const values = Array(5).fill(null);
+        const values = Array(3).fill(null);
         values[0] = 3000;
 
         const [result] = await pool.query<OkPacket>(PROC_SQL, values);
