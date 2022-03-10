550051d10fe1c045e1ba3087308b3dfae1d1515d
cjs8487 <cjschaefer1999@gmail.com>
2022-03-10 17:09:43 -0500

    event api implementation

---
+++ src/__tests__/api/mocks/event.ts
@@ -0,0 +1,113 @@
+import _ from 'lodash';
+import { isAfter, isBefore } from 'date-fns';
+import { Event, PatchEventRequest, PostNewEventRequest } from '../../../typedefs/event';
+import * as event from '../../../database/event';
+
+export const eventList: Event[] = [
+    {
+        eventId: 0,
+        start: '2022-07-12 9:00:00',
+        end: '2022-07-14 22:00:00',
+        eventType: 'race',
+        title: 'Big Testing Event',
+        eventDescription: 'COME ONE COME ALL',
+    },
+    {
+        eventId: 1,
+        start: '2022-08-02 9:00:00',
+        end: '2022-08-04 22:00:00',
+        eventType: 'race',
+        title: 'Big Testing Event 2',
+        eventDescription: 'TO THE BIGGEST TESTING EVENT',
+    },
+    {
+        eventId: 2,
+        start: '2022-09-26 9:00:00',
+        end: '2022-09-28 22:00:00',
+        eventType: 'race',
+        title: 'Big Testing Event 3',
+        eventDescription: 'THE WORLD HAS EVER SEEN',
+    },
+];
+
+export const mockInsertEvent = jest.spyOn(event, 'insertEvent')
+    .mockImplementationOnce((): Promise<number> => {
+        throw new Error('internal server error');
+    }).mockImplementationOnce((): Promise<number> => {
+        throw new Error('user input error');
+    }).mockImplementation((req: PostNewEventRequest): Promise<number> => {
+        let type = '';
+        switch (req.eventTypeId) {
+            case 0:
+                type = 'race';
+                break;
+            default:
+                type = 'a new event';
+        }
+        const newEvent = {
+            eventId: eventList.length,
+            start: req.startDate as string,
+            end: req.endDate as string,
+            eventType: type as string,
+            title: req.eventName as string,
+            eventDescription: req.eventDescription as string,
+        };
+        return Promise.resolve(eventList.push(newEvent) - 1);
+    });
+
+export const mockGetEvent = jest.spyOn(event, 'getEvent')
+    .mockImplementation((id: number): Promise<Event> => {
+        let returnMemeber: Event[] = [];
+        if (id === 400) {
+            throw new Error('internal server error');
+        }
+        returnMemeber = _.filter(eventList, (ev: Event) => ev.eventId === id);
+        if (returnMemeber.length === 0) {
+            throw new Error('not found');
+        }
+        return Promise.resolve(returnMemeber[0]);
+    });
+
+export const mockGetEventList =
+    jest.spyOn(event, 'getEventList').mockImplementationOnce((): Promise<Event[]> => {
+        throw new Error('internal server error');
+    }).mockImplementation((startDate?: string, endDate?: string): Promise<Event[]> => {
+        let events: Event[];
+        // these conditions are a little wonky since ts can't seem to figure out the implicit status of variable types
+        // in else clauses
+        if (typeof startDate === 'undefined' && typeof endDate !== 'undefined') {
+            events = _.filter(eventList, (ev: Event) => isBefore(new Date(ev.start), new Date(endDate)));
+        } else if (typeof endDate === 'undefined' && typeof startDate !== 'undefined') {
+            events = _.filter(eventList, (ev: Event) => isAfter(new Date(ev.start), new Date(startDate)));
+        } else if (typeof startDate !== 'undefined' && typeof endDate !== 'undefined') {
+            events = _.filter(eventList, (ev: Event) => isBefore(new Date(ev.start), new Date(endDate)) &&
+                isAfter(new Date(ev.start), new Date(startDate)));
+        } else {
+            events = eventList;
+        }
+        return Promise.resolve(events);
+    });
+
+export const mockPatchEvent = jest.spyOn(event, 'patchEvent').mockImplementationOnce(() => {
+    throw new Error('internal server');
+}).mockImplementationOnce(async (): Promise<void> => {
+    throw new Error('user input error');
+}).mockImplementation(async (eventId: number, req: PatchEventRequest): Promise<void> => {
+    const filtered = _.filter(eventList, (mem: Event) => mem.eventId === eventId);
+    if (filtered.length === 0) {
+        throw new Error('not found');
+    }
+    eventList[eventId] = {
+        ...eventList[eventId],
+        ...req,
+    };
+});
+
+export const mockDeleteEvent = jest.spyOn(event, 'deleteEvent').mockImplementationOnce(() => {
+    throw new Error('internal server');
+}).mockImplementation(async (eventId: number): Promise<void> => {
+    const deleted = _.remove(eventList, (ev: Event) => ev.eventId === eventId);
+    if (deleted.length === 0) {
+        throw new Error('not found');
+    }
+});
--- src/api/event.ts
+++ src/api/event.ts
@@ -1,25 +1,230 @@
+import { format } from 'date-fns';
 import { Request, Response, Router } from 'express';
+import { deleteEvent, getEvent, getEventList, insertEvent, patchEvent } from '../database/event';
+import {
+    DeleteEventResponse,
+    Event,
+    GetEventListResponse,
+    GetEventResponse,
+    PatchEventResponse,
+    PostNewEventResponse,
+} from '../typedefs/event';
+import { checkHeader, verify } from '../util/auth';
 
 const event = Router();
 
-event.post('/new', (req: Request, res: Response) => {
-    res.status(501).send();
+event.post('/new', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: PostNewEventResponse;
+    const headerCheck = checkHeader(authorization);
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            await verify(headerCheck.token, 'Admin');
+            const insertId = await insertEvent(req.body);
+            response = await getEvent(insertId);
+            res.status(201);
+        } catch (e: any) {
+            if (e.message === 'user input error') {
+                res.status(400);
+                response = { reason: 'bad request' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else if (e.message === 'Forbidden') {
+                res.status(403);
+                response = { reason: 'forbidden' };
+            } else {
+                res.status(500);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
 });
 
-event.get('/list', (req: Request, res: Response) => {
-    res.status(501).send();
+event.get('/list', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: GetEventListResponse;
+    const headerCheck = checkHeader(authorization);
+    const verifyDate = (dateString: string) => {
+        let valid = true;
+        if (dateString === '') {
+            valid = true;
+        }
+        const year = Number(dateString.slice(0, 4));
+        const month = Number(dateString.slice(4, 6));
+        const day = Number(dateString.slice(6, 8));
+        if (Number.isNaN(year) || Number.isNaN(month) || Number.isNaN(day)) {
+            valid = false;
+        }
+        return { valid, date: new Date(year, month - 1, day) };
+    };
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            await verify(headerCheck.token);
+            const { range } = req.headers;
+            let eventList: Event[];
+            if (typeof range === 'undefined') {
+                eventList = await getEventList();
+                res.status(200);
+            } else {
+                const [start, end] = range.split('-');
+                if (start === undefined || end === undefined) {
+                    throw new Error('user input error');
+                }
+                const startData = verifyDate(start);
+                const endData = verifyDate(end);
+                if (!startData.valid || !endData.valid) {
+                    throw new Error('user input error');
+                } else {
+                    if (start === '' && end !== '') {
+                        eventList = await getEventList(
+                            undefined,
+                            `${format(endData.date, 'yyyy-MM-dd')}`,
+                        );
+                    } else if (end === '' && start !== '') {
+                        eventList = await getEventList(`${format(startData.date, 'yyyy-MM-dd')}`);
+                    } else {
+                        eventList = await getEventList(
+                            `${format(startData.date, 'yyyy-MM-dd')}`,
+                            `${format(endData.date, 'yyyy-MM-dd')}`,
+                        );
+                    }
+                    res.status(206);
+                }
+                res.status(206);
+            }
+            response = eventList;
+        } catch (e: any) {
+            if (e.message === 'user input error') {
+                res.status(400);
+                response = { reason: 'bad request' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else {
+                res.status(500);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
 });
 
-event.get('/:eventID', (req: Request, res: Response) => {
-    res.status(501).send();
+event.get('/:eventID', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: GetEventResponse;
+    const headerCheck = checkHeader(authorization);
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            await verify(headerCheck.token);
+            const { eventID } = req.params;
+            const id = Number(eventID);
+            if (Number.isNaN(id)) {
+                throw new Error('not found');
+            }
+            response = await getEvent(id);
+            res.status(200);
+        } catch (e: any) {
+            if (e.message === 'not found') {
+                res.status(404);
+                response = { reason: 'not found' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else {
+                res.status(500);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
 });
 
-event.patch('/:eventID', (req: Request, res: Response) => {
-    res.status(501).send();
+event.patch('/:eventID', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: PatchEventResponse;
+    const headerCheck = checkHeader(authorization);
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            const { eventID } = req.params;
+            const id = Number(eventID);
+            if (Number.isNaN(id)) {
+                throw new Error('not found');
+            }
+            await verify(headerCheck.token, 'Admin');
+            await patchEvent(id, req.body);
+            response = await getEvent(id);
+            res.status(200);
+        } catch (e: any) {
+            if (e.message === 'user input error') {
+                res.status(400);
+                response = { reason: 'bad request' };
+            } else if (e.message === 'not found') {
+                res.status(404);
+                response = { reason: 'not found' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else if (e.message === 'Forbidden') {
+                res.status(403);
+                response = { reason: 'forbidden' };
+            } else {
+                res.status(500);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
 });
 
-event.delete('/:eventID', (req: Request, res: Response) => {
-    res.status(501).send();
+event.delete('/:eventID', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: DeleteEventResponse;
+    const headerCheck = checkHeader(authorization);
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            const { eventID } = req.params;
+            const id = Number(eventID);
+            if (Number.isNaN(id)) {
+                throw new Error('not found');
+            }
+            await verify(headerCheck.token, 'Admin');
+            await deleteEvent(id);
+            response = { eventId: id };
+            res.status(200);
+        } catch (e: any) {
+            if (e.message === 'not found') {
+                res.status(404);
+                response = { reason: 'not found' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else if (e.message === 'Forbidden') {
+                res.status(403);
+                response = { reason: 'forbidden' };
+            } else {
+                res.status(500);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
 });
 
 export default event;
