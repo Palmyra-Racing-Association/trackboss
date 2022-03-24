7dfed8383143fbd75716c09dc958dc1adc8357fc
cjs8487 <cjschaefer1999@gmail.com>
2022-03-24 15:33:41 -0400

    passes with bad test

--- frontend/src/__tests__/snapshots/MemberSummaryModal.test.tsx
+++ frontend/src/__tests__/snapshots/MemberSummaryModal.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import MemberSummaryModal from '../../components/MemberSummaryModal';
 import { Member } from '../../../../src/typedefs/member';
-import { Bike } from '../../../../src/typedefs/bike';
+import { server } from '../../mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 function onClose() {
     // void function
@@ -12,77 +16,25 @@ function onClose() {
 
 const member: Member = {
     memberId: 1,
-    membershipAdmin: 'true',
-    active: true,
-    memberType: 'admin',
-    firstName: 'test',
-    lastName: 'member',
-    phoneNumber: '1234',
-    email: 'user@example.com',
+    membershipId: 1,
+    firstName: 'Squeak',
+    lastName: 'Trainywhel',
+    membershipAdmin: 'Squeak Trainywhel',
     uuid: '',
-    occupation: 'secretary',
-    birthdate: '',
-    dateJoined: '',
-    address: '',
-    city: '',
-    state: '',
-    zip: '',
+    active: true,
+    memberType: 'Admin',
+    phoneNumber: '223-321-4438',
+    occupation: 'Software Engineer',
+    email: 'squeaky@trainingwheels.com',
+    birthdate: '1981-08-13',
+    dateJoined: '2016-10-18',
+    address: '25 Laurel Hill',
+    city: 'North Las Vegas',
+    state: 'NV',
+    zip: '59294',
     lastModifiedDate: '',
     lastModifiedBy: '',
 };
-
-const memberFamily: Member[] = [
-    {
-        memberId: 1,
-        membershipAdmin: 'true',
-        active: true,
-        memberType: 'member',
-        firstName: 'test',
-        lastName: 'member',
-        phoneNumber: '1234',
-        email: 'user@example.com',
-        uuid: '',
-        occupation: '',
-        birthdate: '',
-        dateJoined: '',
-        address: '',
-        city: '',
-        state: '',
-        zip: '',
-        lastModifiedDate: '',
-        lastModifiedBy: '',
-    },
-    {
-        memberId: 2,
-        membershipAdmin: 'string',
-        active: true,
-        memberType: 'member',
-        firstName: 'Jon',
-        lastName: 'Smith',
-        phoneNumber: '1234',
-        email: 'user@example.com',
-        uuid: '',
-        occupation: '',
-        birthdate: '',
-        dateJoined: '',
-        address: '',
-        city: '',
-        state: '',
-        zip: '',
-        lastModifiedDate: '',
-        lastModifiedBy: '',
-    },
-];
-
-const memberBikes: Bike[] = [
-    {
-        bikeId: 0,
-        year: '2012',
-        make: 'honda',
-        model: 'shitbox',
-        membershipAdmin: 'string',
-    },
-];
 
 // Necessary to mock the chakra-ui modal portal, so that the renderer can target the modal instead of the page
 const divWithChildrenMock = (children: any, identifier: any) => <div data-testId={identifier}>{children}</div>;
@@ -94,20 +46,17 @@ jest.mock('@chakra-ui/react', () => (
 ));
 
 describe('member summary modal', () => {
-    it('renders all props correctly', () => {
-        const modal = render(
+    it('renders all props correctly', async () => {
+        const { asFragment } = render((
             <BrowserRouter>
                 <MemberSummaryModal
                     isOpen
                     onClose={onClose}
                     memberInfo={member}
-                    memberFamily={memberFamily}
-                    memberBikes={memberBikes}
                 />
-                ,
-            </BrowserRouter>,
-
-        );
-        expect(modal).toMatchSnapshot();
+            </BrowserRouter>
+        ));
+        await waitFor(() => screen.getByText('Squeak Trainywhel (you)'));
+        expect(asFragment()).toMatchSnapshot();
     });
 });
