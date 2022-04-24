68a2361f12b235aeab67a6593439be20abbe14e2
Alan Delimon <adelimon@gmail.com>
2022-04-23 23:58:05 -0400

    Create an EC2 instance that runs the docker image on the fly and assign to target group.

--- devops/deploy/backend/lib/deploy-stack.ts
+++ devops/deploy/backend/lib/deploy-stack.ts
@@ -8,6 +8,8 @@ import * as cdk from '@aws-cdk/core';
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const account = cdk.Stack.of(this).account;
+    const region = cdk.Stack.of(this).region;
 
     const vpc = ec2.Vpc.fromLookup(this, 'ImportVPC',{isDefault: true});
 
@@ -16,20 +18,23 @@ export class DeployStack extends cdk.Stack {
     });
 
     const listener = alb.addListener('Listener', {
-      port: 81,
+      port: 4443,
       open: true,
       protocol: elbv2.ApplicationProtocol.HTTPS,
       certificates: [elbv2.ListenerCertificate.fromArn('arn:aws:acm:us-east-1:425610073499:certificate/eca2cbf8-247e-4edc-86fb-99c8f28c9dd7')],
     });
 
+    const dockerReg = `${account}.dkr.ecr.${region}.amazonaws.com`;
+    const dockerImg = `${dockerReg}/pra/trackbossapi:latest`;
+
     const userData = ec2.UserData.forLinux();
     userData.addCommands(
       'sudo su',
-      'yum install -y httpd',
       'yum install -y awscli',
-      'systemctl start httpd',
-      'systemctl enable httpd',
-      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
+      'aws s3 cp s3://praconfig/tbenv-test /home/ec2-user',
+      `aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${dockerReg}`,
+      `docker pull ${dockerImg}`,
+      `docker run -p 3000:3000 --env-file /home/ec2-user/tbenv-test ${dockerImg}`,
     );
 
     const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
@@ -38,6 +43,10 @@ export class DeployStack extends cdk.Stack {
         ec2.InstanceClass.T2,
         ec2.InstanceSize.MICRO,
       ),
+      // see https://aws.amazon.com/ec2/pricing/on-demand/ for this number. This should be checked against
+      // instance classes every once in a while to make sure that this is optimal.
+      spotPrice: '0.0084',      
+      keyName: 'prakeyz',
       role: iam.Role.fromRoleName(this, 'ec2-role', 'ec2_aws_access'),
       machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
       userData,
@@ -46,10 +55,11 @@ export class DeployStack extends cdk.Stack {
     });
 
     listener.addTargets('default-target', {
-      port: 80,
+      port: 3000,
+      protocol: elbv2.ApplicationProtocol.HTTP,
       targets: [asg],
       healthCheck: {
-        path: '/',
+        path: '/api/health',
         unhealthyThresholdCount: 2,
         healthyThresholdCount: 5,
         interval: cdk.Duration.seconds(30),
