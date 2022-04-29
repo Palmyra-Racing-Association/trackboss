e58e700f9ca78a6dac1146aa17777d0cd0ffc817
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 <adelimon@gmail.com>
2022-04-28 22:07:57 -0400

    Updated user load and user pool

--- devops/cognito/getuseruuids.sh
+++ devops/cognito/getuseruuids.sh
@@ -2,10 +2,11 @@
 export AWS_PAGER=""
 userPoolId=$1
 csvFile=$2
-
+echo "begin"
 emails=(`cat $csvFile | cut -d ',' -f6`)
 for email in ${emails[@]}; do
   uuid=`aws cognito-idp admin-get-user --user-pool-id us-east-1_8O5sjdZ25 --username $email | jq -r .Username`
   echo "update member set uuid = '$uuid', last_modified_date = now() where email = '$email';";
 done
 echo $emails
+echo "end;\n"
