43abf0f7ab5a3a4a9d296d42c15d35bd51f88789
Alan Delimon <adelimon@gmail.com>
2022-05-06 22:24:01 -0400

    Updated conditional logic in build docker image script as it misses changes.

--- devops/deploy/backend/buildDockerImage.sh
+++ devops/deploy/backend/buildDockerImage.sh
@@ -7,18 +7,13 @@ fi;
 cd $PROJECT_ROOT
 gitCommitId=`git rev-parse --short HEAD`
 backendChanges=`git status -s $PROJECT_ROOT/src`
-if [ -z "$backendChanges" ]
-then
-  echo "No backend changes in commit $gitCommitId, skipping back end Docker creation"
-else 
-  imageName="pra/trackbossapi:latest"
-  imageNameCommit="pra/trackbossapi:$gitCommitId"
-  dockerRegistry="425610073499.dkr.ecr.us-east-1.amazonaws.com"
-  echo "Building docker image for $gitCommitId"
-  docker build --platform linux/amd64 -t $imageName .
-  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $dockerRegistry
-  docker tag $imageName $dockerRegistry/$imageName
-  docker tag $imageName $dockerRegistry/$imageNameCommit
-  docker push $dockerRegistry/$imageName
-  docker push $dockerRegistry/$imageNameCommit
-fi;
+imageName="pra/trackbossapi:latest"
+imageNameCommit="pra/trackbossapi:$gitCommitId"
+dockerRegistry="425610073499.dkr.ecr.us-east-1.amazonaws.com"
+echo "Building docker image for $gitCommitId"
+docker build --platform linux/amd64 -t $imageName .
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $dockerRegistry
+docker tag $imageName $dockerRegistry/$imageName
+docker tag $imageName $dockerRegistry/$imageNameCommit
+docker push $dockerRegistry/$imageName
+docker push $dockerRegistry/$imageNameCommit
