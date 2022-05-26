57c18d18e4ddb7a7a70d9fe1f46e146264b84c99
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 <adelimon@gmail.com>
2022-05-26 13:30:12 -0400

    Search member list

--- frontend/src/components/MemberList.tsx
+++ frontend/src/components/MemberList.tsx
@@ -1,7 +1,8 @@
-import { useDisclosure } from '@chakra-ui/react';
+import { Box, Center, Input, InputGroup, InputLeftElement, useDisclosure } from '@chakra-ui/react';
 import _ from 'lodash';
 import React, { useContext, useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
+import { BsSearch } from 'react-icons/bs';
 import { ErrorResponse } from '../../../src/typedefs/errorResponse';
 import { Member } from '../../../src/typedefs/member';
 import { UserContext } from '../contexts/UserContext';
@@ -57,10 +58,13 @@ const customStyles = {
 export default function MemberList() {
     const [selectedMember, setSelectedMember] = useState<Member>();
     const [cells, setCells] = useState<Member[]>([]);
+    const [allCells, setAllCells] = useState<Member[]>([]);
+
     const { state } = useContext(UserContext);
     const [error, setError] = useState<ErrorResponse | undefined>(undefined);
     const [dirty, setDirty] = useState<boolean>(false);
     const { onClose, isOpen, onOpen } = useDisclosure({ onClose: () => setDirty((oldDirty) => !oldDirty) });
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
     useEffect(() => {
         async function getData() {
@@ -83,12 +87,30 @@ export default function MemberList() {
                         });
                     });
                 }
-                setCells(_.filter(c, (member) => member.active));
+                const activeMembers = _.filter(c, (member) => member.active);
+                setCells(activeMembers);
+                setAllCells(activeMembers);
                 setError(undefined);
             }
         }
         getData();
     }, [dirty]);
+
+    useEffect(() => {
+        if (searchTerm === '') {
+            setCells(allCells);
+        } else {
+            const newCells =
+                allCells.filter((cell: any) => {
+                    const firstNameFound = cell.firstName.toLowerCase().includes(searchTerm);
+                    const lastNameFound = cell.lastName.toLowerCase().includes(searchTerm);
+                    const typeFound = cell.membershipType.toLowerCase().includes(searchTerm);
+                    return (firstNameFound || lastNameFound || typeFound);
+                });
+            setCells(newCells);
+        }
+    }, [searchTerm, allCells]);
+
     if (error) {
         return (
             <div>
@@ -98,6 +120,20 @@ export default function MemberList() {
     }
     return (
         <div>
+            <Center>
+                <Box maxWidth={300}>
+                    <InputGroup>
+                        <InputLeftElement pointerEvents="none">
+                            <BsSearch color="gray.300" />
+                        </InputLeftElement>
+                        <Input
+                            size="lg"
+                            placeholder="Search..."
+                            onChange={(e) => setSearchTerm(e.target.value?.toLowerCase())}
+                        />
+                    </InputGroup>
+                </Box>
+            </Center>
             <DataTable
                 columns={columns}
                 data={cells}
@@ -105,6 +141,7 @@ export default function MemberList() {
                 highlightOnHover
                 pagination
                 responsive
+                striped
                 subHeaderWrap
                 defaultSortFieldId={1}
                 customStyles={customStyles}
