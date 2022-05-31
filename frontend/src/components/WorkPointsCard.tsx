db71f41bb092f8fe6f7d31b09c4f8688c565ae4c
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 <adelimon@gmail.com>
2022-05-31 11:13:03 -0400

    fix over 100% bug

--- frontend/src/components/WorkPointsCard.tsx
+++ frontend/src/components/WorkPointsCard.tsx
@@ -13,6 +13,10 @@ export default function WorkPointsCard(props: cardProps) {
         const path = '/settings';
         navigate(path);
     };
+    let { percent: gaugePercent } = props;
+    if (gaugePercent > 100) {
+        gaugePercent = 100;
+    }
 
     return (
         <Box
@@ -29,20 +33,23 @@ export default function WorkPointsCard(props: cardProps) {
             <Center>
                 <VStack p={0} boxSize="md">
                     <GaugeChart
-                        hideText
                         id="gauge-chart"
                         nrOfLevels={20}
-                        percent={props.percent / 100}
+                        percent={gaugePercent / 100}
                         colors={['#FF9F46', '#76CE6F']}
                         arcWidth={0.3}
                     />
                     <VStack spacing={0}>
-                        <Text pt={5} fontSize="2xl">You have completed</Text>
+                        <Text pt={5} fontSize="xl">You have completed (or signed up for)</Text>
                         <Text color="orange" fontSize="2xl">
                             {props.percent}
                             %
                         </Text>
-                        <Text fontSize="2xl">of this year&apos;s work points</Text>
+                        <Text fontSize="xl">of this year&apos;s work points</Text>
+                        <Text fontSize="xx-small">
+                            Any work that you have been paid for is not included in this total.  This also includes
+                            work that is signed up for but not completed, which will be removed if it is not completed.
+                        </Text>
                     </VStack>
                 </VStack>
             </Center>
