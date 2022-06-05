a7235c6d5bdf7a75a6d1a19e9704c54c71820bc6
Alan Delimon <adelimon@gmail.com>
2022-06-05 00:15:15 -0400

    Added /latest endpoint for gate code connect integration.

--- src/api/gateCode.ts
+++ src/api/gateCode.ts
@@ -5,7 +5,59 @@ import { checkHeader, verify } from '../util/auth';
 import { getPool } from '../database/pool';
 import logger from '../logger';
 
+const getGateCodeByYear = async (year:string) => {
+    const [results] =
+        await getPool().query<RowDataPacket[]>('select * from gate_code where year = ?', [year]);
+    if (results.length === 0) throw new Error('not found');
+    return {
+        id: results[0].gate_code_id,
+        year: results[0].year,
+        gateCode: results[0].gate_code,
+    };
+};
+
+const getGateCodeLatest = async () => {
+    const [results] =
+        await getPool().query<RowDataPacket[]>('select * from gate_code order by year desc limit 1');
+    if (results.length === 0) throw new Error('not found');
+    return {
+        id: results[0].gate_code_id,
+        year: results[0].year,
+        gateCode: results[0].gate_code,
+    };
+};
+
 const gateCode = Router();
+
+gateCode.get('/latest', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: GetGateCodeResponse;
+    const headerCheck = checkHeader(authorization);
+
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            await verify(headerCheck.token);
+            response = await getGateCodeLatest();
+            res.status(200);
+        } catch (e: any) {
+            if (e.message === 'not found') {
+                res.status(404);
+                response = { reason: 'not found' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else {
+                res.status(500);
+                logger.error('error getting latest gate code', e);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
+});
 
 gateCode.get('/:year', async (req: Request, res: Response) => {
     const { authorization } = req.headers;
@@ -19,13 +71,7 @@ gateCode.get('/:year', async (req: Request, res: Response) => {
     } else {
         try {
             await verify(headerCheck.token);
-            const [results] =
-                await getPool().query<RowDataPacket[]>('select * from gate_code where year = ?', [year]);
-            response = {
-                id: results[0].gate_code_id,
-                year: results[0].year,
-                gateCode: results[0].gate_code,
-            };
+            response = await getGateCodeByYear(year);
             res.status(200);
         } catch (e: any) {
             if (e.message === 'not found') {
@@ -59,13 +105,45 @@ gateCode.post('/:year', async (req: Request, res: Response) => {
             const sql =
                 'insert into gate_code (year, gate_code) values (?, ?)';
             const values = [req.body.year, req.body.gateCode];
-            const [results] =
+            const [insertResults] =
                 await getPool().query<RowDataPacket[]>(sql, values);
-            response = {
-                id: results[0].gate_code_id,
-                year: results[0].year,
-                gateCode: results[0].gate_code,
-            };
+            response = await getGateCodeByYear(year);
+            res.status(200);
+        } catch (e: any) {
+            if (e.message === 'not found') {
+                res.status(404);
+                response = { reason: 'not found' };
+            } else if (e.message === 'Authorization Failed') {
+                res.status(401);
+                response = { reason: 'not authorized' };
+            } else {
+                res.status(500);
+                logger.error(`error getting gate code for ${year}`, e);
+                response = { reason: 'internal server error' };
+            }
+        }
+    }
+    res.send(response);
+});
+
+gateCode.put('/:year', async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    let response: PostGateCodeResponse;
+    const headerCheck = checkHeader(authorization);
+    const { year } = req.params;
+
+    if (!headerCheck.valid) {
+        res.status(401);
+        response = { reason: headerCheck.reason };
+    } else {
+        try {
+            await verify(headerCheck.token);
+            const sql =
+                'update gate_code set gate_code = ? where year = ?';
+            const values = [req.body.gateCode, req.body.year];
+            const [updateResults] =
+                await getPool().query<RowDataPacket[]>(sql, values);
+            response = await getGateCodeByYear(year);
             res.status(200);
         } catch (e: any) {
             if (e.message === 'not found') {
