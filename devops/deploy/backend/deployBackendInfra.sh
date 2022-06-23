0b7f9c679c61c6e1edb25a09a55cd1f3215901ca
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 <adelimon@gmail.com>
2022-06-23 16:24:28 -0400

    Add backend CDK to the build process as this was missing before.

--- devops/deploy/backend/deployBackendInfra.sh
+++ devops/deploy/backend/deployBackendInfra.sh
@@ -8,7 +8,5 @@ cd $PROJECT_ROOT/devops/deploy/backend
 npm install
 echo "Deploy backend infrastructure.  Here is the CDK diff..."
 npx cdk diff
-echo "Destroying existing infra - this is the fastest way to reload Docker containers as of 4/2022"
-# npx cdk destroy --force
 npx cdk deploy --require-approval never
-echo "Back end infra is built!  Please manually update any DNS records associated with the change in bluehost"
+echo "Back end infra is built!"
