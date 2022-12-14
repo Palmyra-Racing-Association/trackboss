1c32984d313e936226f8e632c47365df9580d298
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 <adelimon@gmail.com>
2022-12-14 16:27:44 -0500

    Paginate the billing list, and start breaking out search for billing and applictions lists reuse.

---
+++ frontend/src/components/input/DataSearchBox.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Box, Center, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react';
+import { BsBackspace, BsSearch } from 'react-icons/bs';
+
+interface dataSearchBoxProps {
+    searchValue: string,
+    // this eslint rule is a tough guy about the unused var but I like it for clarity so I disabled this rule.
+    // eslint-disable-next-line no-unused-vars
+    onTextChange: (value: string) => void,
+}
+export default function DataSearchBox(props: dataSearchBoxProps) {
+    return (
+        <Center>
+            <Box maxWidth={500}>
+                <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                        <BsSearch color="gray.300" />
+                    </InputLeftElement>
+                    <Input
+                        size="lg"
+                        placeholder="Search..."
+                        value={props.searchValue}
+                        onChange={
+                            (e) => {
+                                props.onTextChange(e.target.value?.toLowerCase());
+                            }
+                        }
+                    />
+                    <InputRightElement
+                        onClick={() => props.onTextChange('')}
+                    >
+                        <BsBackspace color="gray.300" />
+                    </InputRightElement>
+                </InputGroup>
+            </Box>
+        </Center>
+    );
+}
