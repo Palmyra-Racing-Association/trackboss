ba4edbba5d855b05ef1fc1c72d01599da412abbd
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 <adelimon@gmail.com>
2023-04-21 16:11:13 -0400

    #385 - more back end work

--- src/api/memberCommunication.ts
+++ src/api/memberCommunication.ts
@@ -4,6 +4,9 @@ import {
 } from '../database/memberCommunication';
 import { validateAdminAccess } from '../util/auth';
 import logger from '../logger';
+import { getMembersWithTag } from '../database/member';
+import { MemberCommunication } from '../typedefs/memberCommunication';
+import { Member } from '../typedefs/member';
 
 const memberCommunication = Router();
 
@@ -35,8 +38,19 @@ memberCommunication.get('/:communicationId', async (req: Request, res: Response)
 memberCommunication.post('/', async (req: Request, res: Response) => {
     try {
         await validateAdminAccess(req, res);
-        const communication = req.body;
+        const communication : MemberCommunication = req.body;
         const response = await insertMemberCommunication(communication);
+        const uniqueRecipients = new Map<number, Member>();
+        // I want array iteration here because I want things to be in order.
+        // eslint-disable-next-line no-restricted-syntax
+        for (const tag of communication.selectedTags) {
+            // this is on purpose too
+            // eslint-disable-next-line no-await-in-loop
+            const membersWithTags = await getMembersWithTag(tag);
+            membersWithTags.forEach((member) => {
+                uniqueRecipients.set(member.memberId, member);
+            });
+        }
         res.json(response);
     } catch (error: any) {
         logger.error(`Error at path ${req.path}`);
