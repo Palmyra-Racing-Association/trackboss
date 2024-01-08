8763593a69f8d533e82e40cc41fa79118bd37061
Alan Delimon <adelimon@gmail.com>
2024-01-07 23:30:18 -0500

    #430 - Added Knex to manage DB moving forward.

---
+++ database/migrations/knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  production: {
+    client: 'mysql',
+    connection: {
+      database: process.env.MYSQL_DB,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASS,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
