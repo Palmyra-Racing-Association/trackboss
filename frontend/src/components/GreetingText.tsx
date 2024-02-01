927a65a8fb69fad24ddf64c7b8e373874f20db74
Alan Delimon <adelimon@gmail.com>
2024-02-01 00:00:59 -0500

    Revert "Revert "#444 - Show renewals status on dashboard, and some more technical debt collection in packages.""
    This reverts commit 6414fe3b3fa582c6647e5f0bf6bc3670ab2f2e28.

--- frontend/src/components/GreetingText.tsx
+++ frontend/src/components/GreetingText.tsx
@@ -2,22 +2,56 @@ import React from 'react';
 import {
     Center,
     Heading,
+    Highlight,
+    Text,
+    VStack,
 } from '@chakra-ui/react';
 
 interface pageProps {
-    name: string
+    name: string,
+    billYear: number,
+    billPaid?: boolean,
+    insuranceAttested?: boolean,
 }
 
 // the below eslint-disable is needed because of the trailing space in the greeting
-export default function Header(props:pageProps) {
+export default function Header(props: pageProps) {
+    const { billPaid, insuranceAttested, billYear } = props;
+    const allDone = (billPaid && insuranceAttested);
+    let status = (allDone) ? 'complete' : 'incomplete';
+    let highlightColor = 'green';
+    if (!allDone) {
+        highlightColor = 'red';
+        if (!billPaid && !insuranceAttested) {
+            status += '(payment, insurance)';
+        } else if (!billPaid) {
+            status += '(payment)';
+        } else if (!insuranceAttested) {
+            status += '(insurance)';
+        }
+    }
     return (
         <Center>
-            <Heading size="2xl">
-                Hi, {/*eslint-disable-line*/}
-                <Heading sx={{ display: 'inline' }} color="orange" size="2xl">
-                    {props.name}
+            <VStack>
+                <Heading size="2xl">
+                    Hi, {/*eslint-disable-line*/}
+                    <Heading sx={{ display: 'inline' }} color="orange" size="2xl">
+                        {props.name}
+                    </Heading>
                 </Heading>
-            </Heading>
+                <Text fontSize="xl">
+                    <Highlight
+                        query={['complete', 'incomplete']}
+                        styles={{ px: '2', py: '1', rounded: 'full', color: 'white', bg: highlightColor }}
+                    >
+                        {`${billYear + 1} renewal status: ${status}`}
+                    </Highlight>
+                </Text>
+            </VStack>
         </Center>
     );
 }
+Header.defaultProps = {
+    billPaid: true,
+    insuranceAttested: true,
+};
