23db7a94489e5b6c072e0c738cd5252212602a4b
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 <adelimon@gmail.com>
2024-04-17 16:19:39 -0400

    #457 - change back end deployment and only deploy docker image on main build.

--- devops/deploy/backend/redeployApiBackend.sh
+++ devops/deploy/backend/redeployApiBackend.sh
@@ -8,13 +8,5 @@ if [ -z $2 ]; then
   exit -1
 fi;
 
-instance_id=`aws ec2 describe-instances --filters "Name=tag:EnvironmentName,Values=$1" "Name=instance-state-name,Values=running" | jq -r '.Reservations[0].Instances[0].InstanceId'`
-if [ $instance_id == "null" ]; then
-  echo "No instance found for environment $1, leaving it alone for now!"
-else
-  echo $instance_id
-  aws ec2 terminate-instances --instance-ids $instance_id
-fi;
-
 apprunner_arn=`aws apprunner list-services | jq -c '.ServiceSummaryList[] | select( .ServiceName | contains("trackbossRunner"))' | jq -r .ServiceArn`
 aws apprunner start-deployment --service-arn $apprunner_arn
